26737a46cd22052d776e9a02bbc976ec2cd980bd
Gil Ortega <>
2021-05-27 21:39:23 -0700

    Frame Generar Reporte

--- PVGC/src/Test/TestPrincipal.java
+++ PVGC/src/Test/TestPrincipal.java
@@ -18,6 +18,7 @@ import Control.ControlReporteVenta;
 import Control.ControlVenta;
 import Dominio.Categoria;
 import Dominio.DetalleOrden;
+import static Dominio.DetalleOrden_.notas;
 import Dominio.Empleado;
 import Dominio.Orden;
 import Dominio.Producto;
@@ -56,11 +57,11 @@ public class TestPrincipal {
         Categoria cat5 = new Categoria("Desayunos", "Categoría para los platillos para iniciar bien el día");
         
         
-//        cc.agregarCategoria(cat1);
-//        cc.agregarCategoria(cat2);
-//        cc.agregarCategoria(cat3);
-//        cc.agregarCategoria(cat5);
-//        cc.agregarCategoria(cat4);
+        cc.agregarCategoria(cat1);
+        cc.agregarCategoria(cat2);
+        cc.agregarCategoria(cat3);
+        cc.agregarCategoria(cat5);
+        cc.agregarCategoria(cat4);
         
       
        
@@ -69,47 +70,48 @@ public class TestPrincipal {
 //         
 //        //Crear empleados
 //        
-//        Empleado empleado1= new Empleado("Cajero", "Juan Jimenez Mercado", "juanJM_25@gmail.com", "a2asd123dasdasd", "6441923234", "de las mancha #456 entre blvd torre de la viena");
-//        Empleado empleado2= new Empleado("Gerente", "Pedro Montes Jimenez", "PedroMJ_12@gmail.com", "gsgkfks244aksdk", "6442924235", "Fierro viejo #45 entre casa blanca y las fuentes");
-//        
+        Empleado empleado1= new Empleado("Cajero", "Juan Jimenez Mercado", "juanJM_25@gmail.com", "a2asd123dasdasd", "6441923234", "de las mancha #456 entre blvd torre de la viena");
+        Empleado empleado2= new Empleado("Gerente", "Pedro Montes Jimenez", "PedroMJ_12@gmail.com", "gsgkfks244aksdk", "6442924235", "Fierro viejo #45 entre casa blanca y las fuentes");
+        
 //        //Registrarlos en BD
-//        ce.agregarEmpleado(empleado1);
-//        ce.agregarEmpleado(empleado2);
+        ce.agregarEmpleado(empleado1);
+        ce.agregarEmpleado(empleado2);
 //        
 //
 //        // Crear reporte de venta
 //        
-//        ReporteVenta rv1 = new ReporteVenta(Periodo.VESPERTINO, empleado1);
-//        ReporteVenta rv2 = new ReporteVenta(Periodo.MATUTINO, empleado2);
+        ReporteVenta rv1 = new ReporteVenta(Periodo.VESPERTINO, empleado1);
+        ReporteVenta rv2 = new ReporteVenta(Periodo.MATUTINO, empleado2);
 //        
 //        //registrar en BD reporte de venta de empleados
-//        crv.agregarReporteVenta(rv1);
-//        crv.agregarReporteVenta(rv2);
+       crv.agregarReporteVenta(rv1);
+       crv.agregarReporteVenta(rv2);
 //        
 //        //Creación de venta
-//        Calendar fecha = new GregorianCalendar();
+        Calendar fecha = new GregorianCalendar();
 //       
-//        Venta venta1= new Venta(fecha, 324f, rv1);
-//        Venta venta2= new Venta(fecha, 200f, rv2);
+        Venta venta1= new Venta(fecha, 324f, rv1);
+        Venta venta2= new Venta(fecha, 200f, rv2);
+       
 //        
 //        //Registro de venta
-//        cv.agregarVenta(venta1);
-//        cv.agregarVenta(venta2);
+        cv.agregarVenta(venta1);
+        cv.agregarVenta(venta2);
 //        
 //        
 //        EliminarOrden eliminarorden = new EliminarOrden();
 //        
 //       //Creación de orden 
-//        Orden orden1 = new Orden(5, Estado.CONCLUIDO, venta1);
+       Orden orden1 = new Orden(5, Estado.CONCLUIDO, venta1);
 //        
-//        Orden orden2 = new Orden(5,Estado.CONCLUIDO, venta1);
+        Orden orden2 = new Orden(5,Estado.CONCLUIDO, venta1);
 //        
-//        Orden orden3 = new Orden(2, Estado.PROCESANDO, venta2);
+        Orden orden3 = new Orden(2, Estado.PROCESANDO, venta2);
 //       
 //        //registro de ordenes
-//       co.agregarOrden(orden1);
-//       co.agregarOrden(orden2);
-//       co.agregarOrden(orden3);
+       co.agregarOrden(orden1);
+       co.agregarOrden(orden2);
+       co.agregarOrden(orden3);
 //        
 //       List<String> notas = new ArrayList<String>();
 //       String nota1= "Con mucha carne, queso y tocino";
@@ -117,9 +119,9 @@ public class TestPrincipal {
 //       
 //       notas.add(nota1);
 //       notas.add(nota2);
-//        //Creación de detalles de orden
-//        DetalleOrden detOrd1 = new DetalleOrden(orden1, prod1, notas, 2, 80f);
-//        DetalleOrden detOrd2 = new DetalleOrden(orden2, prod2,notas, 1, 35f );
+        //Creación de detalles de orden
+//       DetalleOrden detOrd1 = new DetalleOrden(orden1, prod1, notas, 2, 80f);
+     //  DetalleOrden detOrd2 = new DetalleOrden(orden2, prod2,notas, 1, 35f );
 //        DetalleOrden detOrd3 = new DetalleOrden(orden3, prod3,notas, 3, 120f);
 //        
 //        
@@ -160,10 +162,10 @@ public class TestPrincipal {
 //        cdo.agregarDetalleOrden(detOrd3);
 //        
 //        //lista ordenes para venta
-//        List<Orden> ordenes1 = new ArrayList<Orden>();
-//        ordenes1.add(orden1);
-//        ordenes1.add(orden2);
-//        ordenes1.add(orden3);
+        List<Orden> ordenes1 = new ArrayList<Orden>();
+       ordenes1.add(orden1);
+        ordenes1.add(orden2);
+        ordenes1.add(orden3);
 //        
 //        //Actualizar las ventas con sus ordenes
 //        venta1.setOrdenes(ordenes1);
